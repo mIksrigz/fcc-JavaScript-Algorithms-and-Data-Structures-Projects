9ca97c24cd8e15b24419b7b45d7318ebbafc6db7
Mikolaj <m.iksrigz@gmail.com>
2023-03-04 15:00:37 +0100

    finished palindrome checker

--- palindrome-checker.js
+++ palindrome-checker.js
@@ -1,6 +1,30 @@
+//Program checks if a word/sentence is spelled the same way forwards and backwards,
+//ignoring all non alpha numercic characters, white spaces and letters case.
+//Program returns anwser if input strip of all characters mentioned above is a plindrome.
+//there are some sample tests below.
+
 function palindrome(str) {
-    let preparedString = str.match(/\w+/g).join("").toLowerCase();
-    return preparedString;
+    const preparedString = str.match(/[^\W_+]/g).join("").toLowerCase();
+    let startPointer = 0;
+    let endPointer = preparedString.length - 1;
+    while(startPointer < endPointer){
+        if(preparedString.charAt(startPointer) !== preparedString.charAt(endPointer)) return `${preparedString} is not a plaindrome.`;
+        startPointer++;
+        endPointer--;
+    }
+    return `${preparedString} is a palindrome`;
 }
 
-console.log(palindrome("A man, a plan, a canal. Panama"));
+//tests
+// console.log(palindrome("eye"));
+// console.log(palindrome("_eye"));
+// console.log(palindrome("race car"));
+// console.log(palindrome("not a palindrome"));
+// console.log(palindrome("A man, a plan, a canal. Panama"));
+// console.log(palindrome("never odd or even"));
+// console.log(palindrome("nope"));
+// console.log(palindrome("almostomla"));
+// console.log(palindrome("My age is 0, 0 si ega ym."));
+// console.log(palindrome("1 eye for of 1 eye."));
+// console.log(palindrome("0_0 (: /-\ :) 0-0"));
+// console.log(palindrome("five|\_/|four"));
---
+++ roman-numeral-converter.js
@@ -0,0 +1,65 @@
+// Program takes as an input any integer from 0 to 3999 and returns it as a number in roman numerals;
+// let output = "";
+
+// switch(arabicNumeral){
+//     case 0:
+//         output = "";
+//         break;
+//     case 1:
+//         output = "I";
+//         break;
+//     case 4:
+//         output = "IV";
+//         break;
+//     case 5:
+//         output = "V";
+//         break;
+//     case 9:
+//         output = "IX";
+//         break;
+//     case 10:
+//         output = "X";
+//         break;
+//     case 40:
+//         output = "XL";
+//         break;
+//     case 50:
+//         output = "L";
+//         break;
+//     case 90:
+//         output = "XC";
+//         break;
+//     case 100:
+//         output = "C";
+//         break;
+//     case 400:
+//         output = "CD";
+//         break;
+//     case 500:
+//         output = "D";
+//         break;
+//     case 900:
+//         output = "CM";
+//         break;
+//     case 1000:
+//         output = "M";
+//         break;
+//     default:
+//         output = "Numral not Found";
+//         break;
+// };
+
+function convertToRoman(num) {
+
+   }
+
+//tests
+//console.log(convertToRoman(1549));
+// console.log();
+// console.log();
+// console.log();
+// console.log();
+// console.log();
+// console.log();
+// console.log();
+// console.log();
