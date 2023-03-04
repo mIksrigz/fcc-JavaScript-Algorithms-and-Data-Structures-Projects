86a59f5aa6ccca7b049c7d1c81263e4ad223cd90
Mikolaj <m.iksrigz@gmail.com>
2023-03-04 10:46:17 +0100

    changes in palindrome checker

--- palindrome-checker.js
+++ palindrome-checker.js
@@ -1,1 +1,6 @@
-//file
+function palindrome(str) {
+    let preparedString = str.match(/\w+/g).join("").toLowerCase();
+    return preparedString;
+}
+
+console.log(palindrome("A man, a plan, a canal. Panama"));
