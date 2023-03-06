77ab72331a19771060262b0f7ddc6a2432f6e437
Mikolaj <m.iksrigz@gmail.com>
2023-03-06 10:13:40 +0100

    work in progress

--- cash-register.js
+++ cash-register.js
@@ -1,31 +1,84 @@
 function checkCashRegister(price, cash, cid) {
-	const differenceBetweenPaymentAndPrice = cash - price;
-	const availableCash = cid;
-	let isExactChangePossible = true;
+	const rest = cash - price;
+	let paidRest = 0;
+	const cashRegister = cid.map(array => (
+		{
+			"unitName": array[0],
+			"unitValue": unitNameToValue(array[0]),
+			"unitQuantity": array[1] / unitNameToValue(array[0])
+		}
+	));
+	const cashRegisterStatus = possibleUnits(cashRegister, rest).reverse();
+	const statusObject = { status: "OPEN", change: [] };
 
-	availableCash.forEach(element => {
-		let orderOfCurrency = 0;
-		if (element[0] < differenceBetweenPaymentAndPrice) {
-
+	cashRegisterStatus.forEach(obj => {
+		const quantityAtStart = obj.unitQuantity;
+		while (paidRest <= rest && obj.unitQuantity > 0) {
+			obj.unitQuantity--;
+			paidRest += obj.unitValue;
+			if (paidRest > rest) {
+				obj.unitQuantity++;
+				paidRest -= obj.unitValue;
+				break;
+			}
+		}
+		if (obj.unitQuantity !== quantityAtStart) {
+			statusObject.change.push([obj.unitName, obj.unitValue * (quantityAtStart - obj.unitQuantity)]);
 		}
 	});
-
-	return availableCash;
+	console.log(cashRegisterStatus);
+	return statusObject;
 }
 
-function fromMoneyNameToValue(moneyName) {
+function unitNameToValue(unitName) {
 	let output;
+	switch (unitName) {
+		case "PENNY":
+			output = 0.01;
+			break;
+		case "NICKEL":
+			output = 0.05;
+			break;
+		case "DIME":
+			output = 0.1;
+			break;
+		case "QUARTER":
+			output = 0.25;
+			break;
+		case "ONE":
+			output = 1;
+			break;
+		case "FIVE":
+			output = 5;
+			break;
+		case "TEN":
+			output = 10;
+			break;
+		case "TWENTY":
+			output = 20;
+			break;
+		case "ONE HUNDRED":
+			output = 100;
+			break;
+		default:
+			return "unit not found";
+	}
+	return output;
 }
-console.log(
-	checkCashRegister(19.5, 20, [
-		["PENNY", 1.01],
-		["NICKEL", 2.05],
-		["DIME", 3.1],
-		["QUARTER", 4.25],
-		["ONE", 90],
-		["FIVE", 55],
-		["TEN", 20],
-		["TWENTY", 60],
-		["ONE HUNDRED", 100],
-	])
-);
+
+function possibleUnits(cashRegister, rest) {
+	return cashRegister.filter(unitObject => unitObject["unitValue"] <= rest && unitObject["unitQuantity"] > 0);
+}
+
+// console.log(checkCashRegister(19.5, 20,
+// 	[["PENNY", 1.01],
+// 	["NICKEL", 2.05],
+// 	["DIME", 3.1],
+// 	["QUARTER", 4.25],
+// 	["ONE", 90],
+// 	["FIVE", 55],
+// 	["TEN", 20],
+// 	["TWENTY", 60],
+// 	["ONE HUNDRED", 100]]));
+
+console.log(checkCashRegister(3.26, 100, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]));
