d92a960a61b498aaa1b694c9f0f0132c5fa045ab
Mikolaj <m.iksrigz@gmail.com>
2023-03-08 06:17:47 +0100

    bug fixed

--- cash-register.js
+++ cash-register.js
@@ -1,3 +1,26 @@
+// Program takes as input price, payment and cash in drawer.
+// cash in drawer is an array of smaller arrays, where first
+// index of smaller array is a name of money unit and second
+// index is value of all this units summed up
+// valid input example
+// checkCashRegister(19.5, 20,
+// 	[["PENNY", 1.01],
+// 	["NICKEL", 2.05],
+// 	["DIME", 3.1],
+// 	["QUARTER", 4.25],
+// 	["ONE", 90],
+// 	["FIVE", 55],
+// 	["TEN", 20],
+// 	["TWENTY", 60],
+// 	["ONE HUNDRED", 100]]);
+// program recognizes only money unit names listed above.
+// output is an objec {status, change}
+// if change can be paid status = "OPEN", and change is an array of change in the same format as input
+// if change is exactly equal to cash in drawer status = "CLOSED" and change is all money in drawer
+// if change is greater or cannot be paid status = "INSUFFICIENT_FUNDS"
+// Sample tests below program
+
+
 function checkCashRegister(price, cash, cid) {
 	let change = cash - price;
 	let availableCash = Math.round(cid.reduce((sum, current) => sum + current[1], 0) + "e+2") / 100;
@@ -41,28 +64,27 @@ function checkCashRegister(price, cash, cid) {
 					output = "unitNotFound"
 					break;
 			}
-			return { name: arr[0], value: output, quantity: arr[1] / output, allValue: arr[1] };
+			return { name: arr[0], value: output, quantity: arr[1] / output };
 		})
 		.filter(obj => obj.value <= change && obj.quantity > 0)
 		.reverse();
-	for (let i = 0; i < CashRegister.length; i++) {
-		console.log(change);
-		let recivedChangeValue = 0;
-		while (change - CashRegister[i].value >= 0 && CashRegister[i].quantity > 0) {
-			change -= CashRegister[i].value;
-			CashRegister[i].quantity--;
-			CashRegister[i].allValue -= CashRegister[i].value;
+
+	CashRegister.forEach(obj => {
+		let paidRest = 0;
+		while (change - obj.value >= 0 && obj.quantity > 0) {
+			change -= obj.value;
+			paidRest += obj.value;
+			obj.quantity--;
 			change = Math.round(change + "e+2") / 100;
-			console.log(change);
-			recivedChangeValue += CashRegister[i].value;
 		}
-		if (recivedChangeValue !== 0) {
-			changeArr.push([CashRegister[i].name, recivedChangeValue]);
-		}
-	};
-	if(change > 0) return { status: "INSUFFICIENT_FUNDS", change: [] };
+		if (paidRest > 0) changeArr.push([obj.name, paidRest]);
+	});
+
+	if (change > 0) return { status: "INSUFFICIENT_FUNDS", change: [] };
 	return { status: "OPEN", change: changeArr };
 }
+
+// tests
 
 // console.log(checkCashRegister(19.5, 20,
 // 	[["PENNY", 1.01],
@@ -74,5 +96,47 @@ function checkCashRegister(price, cash, cid) {
 // 	["TEN", 20],
 // 	["TWENTY", 60],
 // 	["ONE HUNDRED", 100]]));
-//console.log(checkCashRegister(3.26, 100, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]));
-//console.log(checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 1], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]));
+
+// console.log(checkCashRegister(3.26, 100,
+// 	[["PENNY", 1.01],
+// 	["NICKEL", 2.05],
+// 	["DIME", 3.1],
+// 	["QUARTER", 4.25],
+// 	["ONE", 90], ["FIVE", 55],
+// 	["TEN", 20],
+// 	["TWENTY", 60],
+// 	["ONE HUNDRED", 100]]));
+
+//  console.log(checkCashRegister(19.5, 20,
+// 	[["PENNY", 0.01],
+// 	["NICKEL", 0],
+// 	["DIME", 0],
+// 	["QUARTER", 0],
+// 	["ONE", 0],
+// 	["FIVE", 0],
+// 	["TEN", 0],
+// 	["TWENTY", 0],
+//  	["ONE HUNDRED", 0]]));
+
+// console.log(checkCashRegister(19.5, 20,
+// 	[["PENNY", 0.01],
+// 	["NICKEL", 0],
+// 	["DIME", 0],
+// 	["QUARTER", 0],
+// 	["ONE", 1],
+// 	["FIVE", 0],
+// 	["TEN", 0],
+// 	["TWENTY", 0],
+// 	["ONE HUNDRED", 0]]));
+
+// console.log(checkCashRegister(19.5, 20,
+// 	[["PENNY", 0.5],
+// 	["NICKEL", 0],
+// 	["DIME", 0],
+// 	["QUARTER", 0],
+// 	["ONE", 0],
+// 	["FIVE", 0],
+// 	["TEN", 0],
+// 	["TWENTY", 0],
+// 	["ONE HUNDRED", 0]])
+// )
